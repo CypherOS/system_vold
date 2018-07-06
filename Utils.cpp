1b7f7d04f2b28b44175e1f37a7af2499acd0d9c1
Chris <chris@cypheros.co>
2018-07-06 01:21:59 -0400

    vold: Maintain fuse exfat when kernel driver is undefined
    We're not forcing kernel exfat on people here and we still ship fuse as
    a working alternative. If the builder has not specified kernel exfat
    support, fallback to fuse
    
    Change-Id: I0d64a990484a90467ce779bd7ed492a55902637a

--- Utils.cpp
+++ Utils.cpp
@@ -540,13 +540,13 @@ bool IsFilesystemSupported(const std::string& fsType) {
         return false;
     }
 
-    /* fuse filesystems */
-    supported.append("fuse\tntfs\n");
-
 #ifdef CONFIG_EXFAT_DRIVER
     /* Add exfat if an exfat driver is present */
     if (supported.find(CONFIG_EXFAT_DRIVER "\n") != std::string::npos)
         supported.append("nodev\texfat\n");
+#else
+    /* fuse filesystems */
+    supported.append("fuse\tntfs\n""fuse\texfat\n");
 #endif
 
     return supported.find(fsType + "\n") != std::string::npos;
